931f7f07f3318d32d99f0bff7217180e2d7252a1
tazarnai <tarik@azarnait.ch>
2011-03-08 15:28:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fischirt::Application.config.secret_token = '6372f49686c017d9734939a2500afaac01cfb83e726aaa456066e53bf9a359ffb38fb99120aaf0c25db4a503a5999efe5d89acc8757320546836d3c6ddc540dd'
