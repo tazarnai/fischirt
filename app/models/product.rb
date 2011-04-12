2c774080d72d989c256bb0ebd6e437e494f62057
Tarik <tarik@azarnait.ch>
2011-04-12 14:16:59 +0200

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,19 @@
 class Product < ActiveRecord::Base
-	
 	default_scope :order => 'title'
-	
+
+  has_many :line_items
+
+  before_destroy :ensure_no_referenced_line_item
+
+  def ensure_no_referenced_line_item
+    if line_items.count.zero?
+      return true
+    else
+      error.add(:base, 'Line Item present')
+      return false
+    end
+  end
+
 	validates :title, :description, :image_url, :presence => true 
 	
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Fischirt::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
